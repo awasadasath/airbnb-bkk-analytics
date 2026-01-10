d959a8d1388797134057e5c59a57887a3a685bcd
Awasada Sathapanachai <awasada.sath@gmail.com>
2026-01-10 14:18:25 +0000

    fix6

--- models/core/mart_sentiment.sql
+++ models/core/mart_sentiment.sql
@@ -7,10 +7,10 @@ WITH reviews AS (
         review_id,
         listing_id,
         review_date,
-        review_text 
-    FROM {{ ref('fct_reviews') }} 
+        review_text
+    FROM {{ ref('fct_reviews') }}
     WHERE review_text IS NOT NULL
-    ORDER BY review_date DESC
+    AND review_date >= '2025-01-01'
     LIMIT 100
 )
 
@@ -18,8 +18,12 @@ SELECT
     review_id,
     listing_id,
     review_date,
-    review_text,
+    review_text AS original_text,
+    
+    SNOWFLAKE.CORTEX.TRANSLATE(review_text, '', 'en') AS translated_text,
+    
     SNOWFLAKE.CORTEX.SENTIMENT(
         SNOWFLAKE.CORTEX.TRANSLATE(review_text, '', 'en')
     ) AS sentiment_score
+
 FROM reviews
